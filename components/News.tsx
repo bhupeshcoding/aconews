a1354164412355f31bc634931b1e785c64a5d4d0
bhupeshcoding <bhupesh.coding@gmail.com>
2024-10-13 13:19:59 +0530

    your commit

--- components/News.tsx
+++ components/News.tsx
@@ -262,6 +262,18 @@
 // //     </div>
 // //   );
 // // };
+// echo "# aconews" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/bhupeshcoding/aconews.git
+// git push -u origin main
+
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/bhupeshcoding/aconews.git
+// git branch -M main
+// git push -u origin main
 
 // // export default News;
 
